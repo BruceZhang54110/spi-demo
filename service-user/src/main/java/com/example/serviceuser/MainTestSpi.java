22711994a3ac603410966d22fc0e3b28b18d978f
BruceZhang54110 <zhw54110@163.com>
2025-03-01 23:47:04 +0800

    update

--- service-user/src/main/java/com/example/serviceuser/MainTestSpi.java
+++ service-user/src/main/java/com/example/serviceuser/MainTestSpi.java
@@ -2,14 +2,17 @@ package com.example.serviceuser;
 
 import com.example.service.Perform;
 
+import java.util.Iterator;
 import java.util.ServiceLoader;
 
 public class MainTestSpi {
     public static void main(String[] args) {
-        ServiceLoader<Perform> load = ServiceLoader.load(Perform.class);
-        for (Perform perform : load) {
-            System.out.println("遍历 start");
+        ServiceLoader<Perform> serviceLoader = ServiceLoader.load(Perform.class);
+        Iterator<Perform> iterator = serviceLoader.iterator();
+        while (iterator.hasNext()) {
+            Perform perform = iterator.next();
             perform.show();
         }
+
     }
 }
